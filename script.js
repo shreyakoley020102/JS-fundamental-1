d9fae03da2952efc944fcd76009b8037194a43c1
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2020-12-25 20:46:28 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ let js = 'amazing';
 if (js === 'amazing') alert('Javascript is fun');
 
 console.log(40+8+0-78);
+console.log('Shreya');
+
+let firstName = "Shreya";
+console.log(firstName);
+
+
+/*
+let javascriptisfun = true;
+console.log(javascriptisfun);
+
+console.log(typeof true);
+console.log(typeof javascriptisfun);
+console.log(typeof 34);
+console.log(typeof 'Shreya');
+*/
